1173d9c8ee7db736c0b47e9847a0334337f3f13a
PierreAlexisPAUROBALLY <pa.paurobally@gmail.com>
2022-05-15 19:26:51 +0200

    commit for Submition
    commit 2

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
+import {PoseidonT3} from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
-
+import "hardhat/console.sol";
 contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
@@ -11,19 +11,95 @@ contract MerkleTree is Verifier {
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        // we could also precompute the hashes and set them manually since they are always going to be the same.
+        hashes = [0, 0, 0, 0, 0, 0, 0, 0];
+
+        for (uint256 i = 0; i < 7; i++) {
+            uint256 interm_hash = PoseidonT3.poseidon(
+                [hashes[2 * i], hashes[2 * i + 1]]
+            );
+            hashes.push(interm_hash);
+        }
+        root = hashes[14];
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+
+        //completely unoptimised since we relcaculate the wole tree each time. 
+        // [todo] rewrite
+
+        hashes[index] = hashedLeaf;
+        uint256 hix = 8;
+
+        for (uint256 i = 0; i < 7; i++) {
+            uint256 interm_hash = PoseidonT3.poseidon(
+                [hashes[2 * i], hashes[2 * i + 1]]
+            );
+            hashes[hix] = interm_hash;
+            hix++;
+        }
+
+        index++;
+        root = hashes[14];
+        return root;
     }
 
-    function verify(
-            uint[2] memory a,
-            uint[2][2] memory b,
-            uint[2] memory c,
-            uint[1] memory input
-        ) public view returns (bool) {
+    function _insertLeaf(uint256 hashedLeaf) public returns (uint256) {
 
+        uint depth=3;//hardset here, can easily be modified for another depth
+        uint Lix=0;//ix of 1st leaf at current depth
+        uint nodeToModifIncrement=index;// ix of leaf to modify at current depth minus Lix
+        uint Mix = index; // last modified leaf (NTM from round -1)
+        uint leafsAtDepth=0;
+        uint NTM=index;// node to modify index
+
+        
+
+        for (uint i = depth ; i!=0 ; i--){///iter from leafs to root
+            //console.log(i);
+            leafsAtDepth = 2**i;
+
+            if (i == depth){//init loop
+                hashes[index]= hashedLeaf;
+            }
+            else{
+            NTM= Lix + nodeToModifIncrement;// node to modify
+            //console.log('NTM',NTM);
+            //console.log('LIX',Lix);
+
+            if(Mix%2==0){// if the node to modify (Mix) is %2 then we take the Mix+1 else Mix-1; also need to hash in order 
+                hashes[NTM]=PoseidonT3.poseidon([hashes[Mix], hashes[Mix+1]]);
+            }
+            else{
+                hashes[NTM]=PoseidonT3.poseidon([hashes[Mix-1], hashes[Mix]]);
+            }
+            Mix=NTM;// for next round, Modified Index is current node to modify
+            }
+
+            
+            nodeToModifIncrement= nodeToModifIncrement/2;// the increment of nodetomodify per depth (ex: 1 0 0 [ 1 , 8+0,12+0] ; 3 1 0 ; 7 3 1 [7, 8+3 , 12+1])
+            Lix=Lix + leafsAtDepth;// Last Index (first element in Hasharray for next round)
+            
+        }
+
+        root=hashes[14];// last element Lix
+        return root;
+    }
+
+
+
+    
+
+
+    function verify(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[1] memory input
+    ) public view returns (bool) {
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+
+        return (Verifier.verifyProof(a, b, c, input) && root == input[0]);
     }
 }
--- custom.test.modified.js
+++ custom.test.modified.js
@@ -1,1 +1,213 @@
-// [assignment] please copy the entire modified custom.test.js here
+const hre = require('hardhat')
+const { ethers, waffle } = hre
+const { loadFixture } = waffle
+const { expect } = require('chai')
+const { utils } = ethers
+
+const Utxo = require('../src/utxo')
+const { transaction, registerAndTransact, prepareTransaction, buildMerkleTree } = require('../src/index')
+const { toFixedHex, poseidonHash } = require('../src/utils')
+const { Keypair } = require('../src/keypair')
+const { encodeDataForBridge } = require('./utils')
+const config = require('../config')
+const { generate } = require('../src/0_generateAddresses')
+
+const MERKLE_TREE_HEIGHT = 5
+const l1ChainId = 1
+const MAXIMUM_DEPOSIT_AMOUNT = utils.parseEther(process.env.MAXIMUM_DEPOSIT_AMOUNT || '1')
+
+describe('TornadoPool', function () {
+  this.timeout(20000)
+
+  async function deploy(contractName, ...args) {
+    const Factory = await ethers.getContractFactory(contractName)
+    const instance = await Factory.deploy(...args)
+    return instance.deployed()
+  }
+
+  async function fixture() {
+    require('../scripts/compileHasher')
+    const [sender, gov, multisig] = await ethers.getSigners()
+    const verifier2 = await deploy('Verifier2')
+    const verifier16 = await deploy('Verifier16')
+    const hasher = await deploy('Hasher')
+
+    const token = await deploy('PermittableToken', 'Wrapped ETH', 'WETH', 18, l1ChainId)
+    await token.mint(sender.address, utils.parseEther('10000'))
+
+    const l1Token = await deploy('WETH', 'Wrapped ETH', 'WETH')
+    await l1Token.deposit({ value: utils.parseEther('3') })
+
+    const amb = await deploy('MockAMB', gov.address, l1ChainId)
+    const omniBridge = await deploy('MockOmniBridge', amb.address)
+
+    // deploy L1Unwrapper with CREATE2
+    const singletonFactory = await ethers.getContractAt('SingletonFactory', config.singletonFactory)
+
+    let customConfig = Object.assign({}, config)
+    customConfig.omniBridge = omniBridge.address
+    customConfig.weth = l1Token.address
+    customConfig.multisig = multisig.address
+    const contracts = await generate(customConfig)
+    await singletonFactory.deploy(contracts.unwrapperContract.bytecode, config.salt)
+    const l1Unwrapper = await ethers.getContractAt('L1Unwrapper', contracts.unwrapperContract.address)
+
+    /** @type {TornadoPool} */
+    const tornadoPoolImpl = await deploy(
+      'TornadoPool',
+      verifier2.address,
+      verifier16.address,
+      MERKLE_TREE_HEIGHT,
+      hasher.address,
+      token.address,
+      omniBridge.address,
+      l1Unwrapper.address,
+      gov.address,
+      l1ChainId,
+      multisig.address,
+    )
+
+    const { data } = await tornadoPoolImpl.populateTransaction.initialize(MAXIMUM_DEPOSIT_AMOUNT)
+    const proxy = await deploy(
+      'CrossChainUpgradeableProxy',
+      tornadoPoolImpl.address,
+      gov.address,
+      data,
+      amb.address,
+      l1ChainId,
+    )
+
+    const tornadoPool = tornadoPoolImpl.attach(proxy.address)
+
+    await token.approve(tornadoPool.address, utils.parseEther('10000'))
+
+    return { tornadoPool, token, proxy, omniBridge, amb, gov, multisig, l1Unwrapper, sender, l1Token }
+  }
+
+  it('alice deposits L1 withdraw L2', async () => {
+    const {tornadoPool,token, omniBridge } = await loadFixture(fixture)
+    
+    const aliceKeyPair = new Keypair()
+    const aliceDepositAmount = utils.parseEther("0.1")
+    const aliceDepositUtxo = new Utxo({ amount: aliceDepositAmount, keypair: aliceKeyPair })
+
+
+    const { args, extData } = await prepareTransaction({tornadoPool,outputs: [aliceDepositUtxo],})
+    const onTokenBridgedData = encodeDataForBridge({proof: args,extData,})
+
+    const onTokenBridgeTx = await tornadoPool.populateTransaction.onTokenBridged(token.address, aliceDepositUtxo.amount, onTokenBridgedData)
+
+    await token.transfer(omniBridge.address, aliceDepositAmount)
+    const transferTx = await token.populateTransaction.transfer(tornadoPool.address, aliceDepositAmount)
+
+    await omniBridge.execute([
+      { who: token.address, callData: transferTx.data }, // send tokens to pool
+      { who: tornadoPool.address, callData: onTokenBridgeTx.data }, // call onTokenBridgedTx
+    ])
+
+    const withdrawAmount=utils.parseEther('0.08')
+    const withdrawAdress='0xDeaD00000000000000000000000000000000BEEf'
+    const withdrawUtxo= new Utxo({amount:aliceDepositAmount.sub(withdrawAmount),keypair:aliceKeyPair})
+    await transaction({
+      tornadoPool,inputs:[aliceDepositUtxo],outputs:[withdrawUtxo],recipient:withdrawAdress
+    })
+    const recipientBalance= await token.balanceOf(withdrawAdress) // withrew 0.08
+    expect(recipientBalance).to.be.equal(withdrawAmount)
+
+    const omniBridgeBalance = await token.balanceOf(omniBridge.address) // on L2 nothing on bridge
+    expect(omniBridgeBalance).to.be.equal(0)
+
+    const TornadoPoolBalance = await token.balanceOf(tornadoPool.address) // the not withdrew stuff left in the liquiditypool
+    expect (TornadoPoolBalance).to.be.equal(withdrawUtxo.amount)
+
+
+  })
+
+  it('A deposit send to B, B withdraw A withdraw',async() =>{
+    const {tornadoPool,token, omniBridge } = await loadFixture(fixture)
+    
+    const aliceKeyPair = new Keypair()
+    const aliceDepositAmount = utils.parseEther('0.13')
+    const aliceDepositUtxo = new Utxo({ amount: aliceDepositAmount, keypair: aliceKeyPair })
+
+    const { args, extData } = await prepareTransaction({tornadoPool,outputs: [aliceDepositUtxo],})
+    const onTokenBridgedData = encodeDataForBridge({proof: args,extData,})
+
+    const onTokenBridgeTx = await tornadoPool.populateTransaction.onTokenBridged(token.address, aliceDepositUtxo.amount, onTokenBridgedData)
+
+    await token.transfer(omniBridge.address, aliceDepositAmount)
+    const transferTx = await token.populateTransaction.transfer(tornadoPool.address, aliceDepositAmount)
+
+    await omniBridge.execute([
+      { who: token.address, callData: transferTx.data }, // send tokens to pool
+      { who: tornadoPool.address, callData: onTokenBridgeTx.data }, // call onTokenBridgedTx
+    ])
+
+    // Bob gives Alice address to send some eth inside the shielded pool
+    const bobKeypair = new Keypair() // contains private and public keys
+    const bobAddress = bobKeypair.address() // contains only public key
+
+    // Alice sends some funds to Bob in L2
+    const bobSendAmount = utils.parseEther('0.06')
+    const bobSendUtxo = new Utxo({ amount: bobSendAmount, keypair: Keypair.fromString(bobAddress) })
+    const aliceChangeUtxo = new Utxo({
+      amount: aliceDepositAmount.sub(bobSendAmount),
+      keypair: aliceDepositUtxo.keypair,
+    })
+
+    await transaction({ tornadoPool, inputs: [aliceDepositUtxo], outputs: [bobSendUtxo, aliceChangeUtxo] })
+
+    // bob withdraw in L2
+    // Bob parses chain to detect incoming funds
+    const filter = tornadoPool.filters.NewCommitment()
+    const fromBlock = await ethers.provider.getBlock()
+    const events = await tornadoPool.queryFilter(filter, fromBlock.number)
+    let bobReceiveUtxo
+    try {
+      bobReceiveUtxo = Utxo.decrypt(bobKeypair, events[0].args.encryptedOutput, events[0].args.index)
+    } catch (e) {
+      // we try to decrypt another output here because it shuffles outputs before sending to blockchain
+      bobReceiveUtxo = Utxo.decrypt(bobKeypair, events[1].args.encryptedOutput, events[1].args.index)
+    }
+
+
+    const bobWithdrawUtxo= new Utxo ({amount: 0,keypair:bobKeypair})// bob want to withdraw all
+    const bobWithdrawAddress='0x0DB143eDe6805F23922535Bad7Acb3e9Aa5D2F7b'
+    await transaction({
+      tornadoPool,inputs:[bobReceiveUtxo],outputs:[bobWithdrawUtxo],recipient:bobWithdrawAddress
+    })// the balance on bobWithdrawAddress should be 0.06
+
+    // Alice withdraws in L1
+    const aliceWithdrawAddress='0x1D59B58B9Ba1CB87c40024e92e341C15cC5ce2F0'
+    const aliceWithdrawUtxo=new Utxo({amount : 0,keypair:aliceKeyPair})// alice want to withdraw all
+    await transaction({tornadoPool,inputs:[aliceChangeUtxo],outputs:[aliceWithdrawUtxo],recipient:aliceWithdrawAddress,isL1Withdrawal:true,})
+    // the balance of the address shgould be 0 , the address of the omibridge sgould be 0.13-0.06
+
+
+    const bobBalance= await token.balanceOf(bobWithdrawAddress) // withrew 0.08 on L2
+    expect(bobBalance).to.be.equal(bobReceiveUtxo.amount)
+
+    const aliceBalance = await token.balanceOf(aliceWithdrawAddress) // withdrew on L1 nothing left here
+    expect(aliceBalance).to.be.equal(0)
+
+
+    const omniBridgeBalance = await token.balanceOf(omniBridge.address) 
+    expect(omniBridgeBalance).to.be.equal(aliceChangeUtxo.amount) // on bridge there should be residual alice balance (0.13-0.06)
+    expect(omniBridgeBalance).to.be.equal(utils.parseEther('0.07'))
+
+    const TornadoPoolBalance = await token.balanceOf(tornadoPool.address) // nothing left in so should be 0
+    expect (TornadoPoolBalance).to.be.equal(0)
+
+
+
+
+
+
+
+
+
+
+  })
+
+})
+
